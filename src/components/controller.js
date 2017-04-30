9f413258dc4c9e6e8ee3ebdbd869d911a29563a4
Yin Sheng Zhou <yinsheng@sg.ibm.com>
2017-04-30 14:21:26 +0800

    commit before test

--- src/components/controller.js
+++ src/components/controller.js
@@ -1,56 +1,58 @@
-angular.module('app',[
-         'ngAnimate',
-         'ngRoute',
-         'ngSanitize',
-         'ngTouch',
-         'ui.router',
-         'ui.bootstrap',
-         'ngStorage',
-         'patternUnlock',
-       ])
-       .config(function($stateProvider, $urlRouterProvider) {
-         $urlRouterProvider
-           .otherwise('/app/index');
+(function() {
+    "use strict";
 
-         $stateProvider
-           .state('app', {
-             abstract: true,
-             url: '/app',
-             templateUrl: 'components/common/views/index.html',
-             controller: 'AppCtrl',
-             controllerAs: 'app'
-           })
-           .state('app.index', {
-             url: '/index',
-             templateUrl: 'components/common/views/lock.html'
-           })
-           .state('app.unlock', {
-             url: '/unlock',
-             templateUrl: 'components/common/views/unlock.html'
-           })
-       })
-       .controller('AppCtrl', [
-           'PatternUnlockService',
-           '$state',
-           '$localStorage',
-           function (PatternUnlockService, $state, $localStorage) {
-               var vm = this;
+    angular.module('app',[
+        'ngAnimate',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'ui.router',
+        'ui.bootstrap',
+        'ngStorage',
+        'patternUnlock',
+    ])
+        .config(function($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider
+                .otherwise('/app/index');
 
-               vm.greeting = 'Hello world';
+            $stateProvider
+                .state('app', {
+                    abstract: true,
+                    url: '/app',
+                    templateUrl: 'components/common/views/index.html',
+                    controller: 'AppCtrl',
+                    controllerAs: 'app'
+                })
+                .state('app.index', {
+                    url: '/index',
+                    templateUrl: 'components/common/views/lock.html'
+                })
+                .state('app.unlock', {
+                    url: '/unlock',
+                    templateUrl: 'components/common/views/unlock.html'
+                })
+        })
+        .controller('AppCtrl', [
+            'PatternUnlockService',
+            '$state',
+            '$localStorage',
+            function (PatternUnlockService, $state, $localStorage) {
+                var vm = this;
 
-               vm.reset = function() {
-                   PatternUnlockService.reset();
-               }
+                vm.reset = function() {
+                    PatternUnlockService.reset();
+                }
 
-               vm.unlock = function() {
-                   PatternUnlockService.unlock()
-                       .then(function() {// Success
-                           $state.go('app.unlock');
-                       });
-               }
+                vm.unlock = function() {
+                    PatternUnlockService.unlock()
+                        .then(function() {// Success
+                            $state.go('app.unlock');
+                        });
+                }
 
-               if (!PatternUnlockService.isRegistered()) {
-                   PatternUnlockService.reset();
-               }
-           }
-       ]);
+                if (!PatternUnlockService.isRegistered()) {
+                    PatternUnlockService.reset();
+                }
+            }
+        ]);
+})();
--- src/components/patternUnlock/unlock.service.js
+++ src/components/patternUnlock/unlock.service.js
@@ -1,55 +1,59 @@
-angular.module('patternUnlock', [])
-    .service('PatternUnlockService', [
-        '$uibModal', 
-        '$localStorage',
-        function ($uibModal, $localStorage) {
-            this.isRegistered = function() {
-                return !!$localStorage.pattern;
-            }
+(function() {
+    "use strict";
 
-            this.reset = function() {
-                // return uibModal.open's promise
-                return $uibModal.open({
-                    templateUrl: 'components/patternUnlock/views/reset.modal.html',
-                    controller: function($scope, $localStorage, $uibModalInstance) {
-                        $scope.errMessage = false;
+    angular.module('patternUnlock', [])
+        .service('PatternUnlockService', [
+            '$uibModal', 
+            '$localStorage',
+            function ($uibModal, $localStorage) {
+                this.isRegistered = function() {
+                    return !!$localStorage.pattern;
+                }
 
-                        $scope.validate = function(pattern) {
-                            if (pattern.length < 4) {
-                                $scope.errMessage = true;
-                            } else {
-                                $localStorage.pattern = pattern;
-                                $uibModalInstance.close();
+                this.reset = function() {
+                    // return uibModal.open's promise
+                    return $uibModal.open({
+                        templateUrl: 'components/patternUnlock/views/reset.modal.html',
+                        controller: function($scope, $localStorage, $uibModalInstance) {
+                            $scope.errMessage = false;
+
+                            $scope.validate = function(pattern) {
+                                if (pattern.length < 4) {
+                                    $scope.errMessage = true;
+                                } else {
+                                    $localStorage.pattern = pattern;
+                                    $uibModalInstance.close();
+                                }
                             }
-                        }
-                    },
-                    keyboard: false,
-                    backdrop: 'static',
-                    size: 'lg',
-                    windowClass: 'modal-fullscreen'
-                }).result;
-            }
+                        },
+                        keyboard: false,
+                        backdrop: 'static',
+                        size: 'lg',
+                        windowClass: 'modal-fullscreen'
+                    }).result;
+                }
 
-            this.unlock = function() {
-                // return uibModal.open's promise
-                return $uibModal.open({
-                    templateUrl: 'components/patternUnlock/views/unlock.modal.html',
-                    controller: function($scope, $localStorage, $uibModalInstance) {
-                        $scope.isIncorrect = false;
+                this.unlock = function() {
+                    // return uibModal.open's promise
+                    return $uibModal.open({
+                        templateUrl: 'components/patternUnlock/views/unlock.modal.html',
+                        controller: function($scope, $localStorage, $uibModalInstance) {
+                            $scope.isIncorrect = false;
 
-                        $scope.validate = function(pattern) {
-                            if (angular.equals(pattern, $localStorage.pattern)) {
-                                $uibModalInstance.close()
-                            } else {
-                                $scope.isIncorrect = true;
+                            $scope.validate = function(pattern) {
+                                if (angular.equals(pattern, $localStorage.pattern)) {
+                                    $uibModalInstance.close()
+                                } else {
+                                    $scope.isIncorrect = true;
+                                }
                             }
-                        }
-                    },
-                    keyboard: false,
-                    backdrop: 'static',
-                    size: 'lg',
-                    windowClass: 'modal-fullscreen'
-                }).result;
+                        },
+                        keyboard: false,
+                        backdrop: 'static',
+                        size: 'lg',
+                        windowClass: 'modal-fullscreen'
+                    }).result;
+                }
             }
-        }
-    ]);
+        ]);
+})();
